174198836f533373e354c30d15e12a9cf9f078d0
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-07-29 20:57:33 +0530

    update

--- auto-update (1).sh
+++ auto-update (1).sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# cd desktop/vs_code
+cd desktop/vs_code
 # cd $(dirname ${BASH_SOURCE[0]})
 
 
 while true
 do  
     # echo "root@2022" | sudo -S -k purge
-    # sleep 120
+    sleep 120
     if [[ -n $(git status -s) ]]; then
         echo "Changes found. Pushing changes..."
         git add -A && git commit -m 'update' && git push
