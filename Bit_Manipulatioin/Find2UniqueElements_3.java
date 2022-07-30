f78e4aed8425df98aad65cbd6044daef8adc22c0
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-07-30 12:45:10 +0530

    update

--- Bit_Manipulatioin/Find2UniqueElements_3.java
+++ Bit_Manipulatioin/Find2UniqueElements_3.java
@@ -4,7 +4,7 @@ public class Find2UniqueElements_3 {
     public static void main(String[] args) {
         System.out.println("babaji");
         int[] arr = {1,2,3,4,6,2,1,3,4,5};
-        System.out.println(arr[2]<<1);
+        System.out.println(4>>0);
         // solve(arr);
     }
     static void solve(int[] arr){
