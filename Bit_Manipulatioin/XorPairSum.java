0bc15fc47f8c01f292acf5bf14bc0647482e0905
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-02 23:58:57 +0530

    update

--- Bit_Manipulatioin/XorPairSum.java
+++ Bit_Manipulatioin/XorPairSum.java
@@ -4,6 +4,9 @@ public class XorPairSum {
     public static void main(String[] args) {
         System.out.println("babaji");
         String s = "Rama";
-        String s1 = (String) s.charAt(2);
+        String s1 = s.substring(1);
+        System.out.println(s1);
+        String s2 = s1.substring(1);
+        
     }
 }
