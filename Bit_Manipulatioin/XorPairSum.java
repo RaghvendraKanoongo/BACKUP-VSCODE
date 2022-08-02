2dd10d87319d3225dd7cf3de551d6b3d97d920c4
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-03 00:34:25 +0530

    update

--- Bit_Manipulatioin/XorPairSum.java
+++ Bit_Manipulatioin/XorPairSum.java
@@ -5,7 +5,7 @@ public class XorPairSum {
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
         // String s = sc.nextLine();
-        String s = "Ram";
+        String s = "RamayRamchandraya";
         // int sLen = s.length();
         System.out.println(solve(s));
     }
@@ -15,7 +15,7 @@ public class XorPairSum {
         if(s.length() == 1){
             return "";
         }
-        System.out.println("ji");
+
         return solve(""+solve(s.substring(1))+s.charAt(0));
 
     }
