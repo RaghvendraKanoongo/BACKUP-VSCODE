84456f2db9d6700976e2d7ce09ee05a344f7bada
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-04 19:17:58 +0530

    update

--- SortingAlgos/Selection.java
+++ SortingAlgos/Selection.java
@@ -5,7 +5,8 @@
  * Selection
  */
 
- // find the minimum value and put it into the starting index 
+ // find the minimum value and put it into the starting index next iteration
+ // should start from i++;
 
 public class Selection {
 
