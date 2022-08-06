0de9c8ced70df6e5bc25ca4429fe8286f2d23b11
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-07 00:03:30 +0530

    update

--- OLD/Java/hola.java
+++ OLD/Java/hola.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class hola {
 
   public static void main(String[] args) {
-    ArrayList list
+    ArrayList list = new ArrayList<>();
+    list.add("hola");
+    list.add(10);
+    System.out.println(list);
   }
 }
