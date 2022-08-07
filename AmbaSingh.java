c0b9fc6b4763717d1f45b1735721f948790c8db2
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-07 14:10:02 +0530

    update

--- AmbaSingh.java
+++ AmbaSingh.java
@@ -4,18 +4,13 @@ public class AmbaSingh {
         x = 98;
     }
     static int x = 99;
-    static final int zz;
-    AmbaSingh(int zz){
-        super();
-            this.zz = zz;
-            System.out.println(this.zz);
-    }
-    public static void main(String[] args) {
-        System.out.println("Babaji");
-        System.out.println(x);
-        int y = 99;
-        AmbaSingh as = new AmbaSingh(55);
 
+    
+    public static void main(String[] args) {
+        final int i  = 0;
+        System.out.println("chal bha");
+
+        System.out.println(i++);
         
 
     }
