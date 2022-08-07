b03e1170126dfc2b9a39383512d590d660e43687
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-07 13:25:19 +0530

    update

--- AmbaSingh.java
+++ AmbaSingh.java
@@ -1,6 +1,13 @@
-package com.AmbaSingh;
+// package com.AmbaSingh;
 public class AmbaSingh {
+    static {
+        x = 98;
+    }
+    static int x = 99;
+        
     public static void main(String[] args) {
         System.out.println("Babaji");
+        System.out.println(x);
+
     }
 }
