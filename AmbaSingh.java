77d5f2bb2796b916b9b1b07875c71555699cba54
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-07 13:59:55 +0530

    update

--- AmbaSingh.java
+++ AmbaSingh.java
@@ -4,11 +4,16 @@ public class AmbaSingh {
         x = 98;
     }
     static int x = 99;
-        
+    final int zz;
+    AmbaSingh(int zz){
+            this.zz = zz;
+    }
     public static void main(String[] args) {
         System.out.println("Babaji");
         System.out.println(x);
         int y = 99;
+        AmbaSingh
+        
 
     }
 }
