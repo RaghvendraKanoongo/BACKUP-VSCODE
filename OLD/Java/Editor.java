c019ebb4afd85cf84e955300cb9bf7b733267613
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-07 12:57:13 +0530

    update

--- OLD/Java/Editor.java
+++ OLD/Java/Editor.java
@@ -1,11 +1,12 @@
 import java.lang.Integer;
 // import static java.lang.Byte.MAX_VALUE;
+import static java.lang.System.*;
 import java.lang.Math;
 public class Editor {
 
     // static int MAX_VALUE = 99;
     public static void main(String args[]){
-        
+        out.println("babaji");
         
     }
     
