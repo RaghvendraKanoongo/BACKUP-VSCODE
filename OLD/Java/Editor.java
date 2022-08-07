cdc7d857141c071bfd78f998f76c30880171dfe7
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-07 12:53:10 +0530

    update

--- OLD/Java/Editor.java
+++ OLD/Java/Editor.java
@@ -1,10 +1,11 @@
-import static java.lang.Integer.MAX_VALUE;;
-import static java.lang.Byte.MAX_VALUE;;
+import static java.lang.Integer.MAX_VALUE;
+// import static java.lang.Byte.MAX_VALUE;
+import java.lang.Math;
 public class Editor {
 
     // static int MAX_VALUE = 99;
     public static void main(String args[]){
-        System.out.println(MAX_VALUE);
+        
         
     }
     
