0837d8e60664ba3df30179c6fd61278196cd5e79
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-09 00:13:19 +0530

    update

--- SortingAlgos/MergeSort.java
+++ SortingAlgos/MergeSort.java
@@ -4,5 +4,15 @@ public class MergeSort {
     public static void main(String[] args) {
         System.out.println("babaji");
     }
-    
+
+}
+
+class A{
+    static void hola(){
+        System.out.println("in static A");
+    }
+    void hola1(){
+        System.out.println("In non static A");
+    }
+
 }
