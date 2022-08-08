6f060842c53a00378713cfd22ffbbf0e11209cac
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-09 01:11:38 +0530

    update

---
+++ AAAAA/UnderstandingInterf.java
@@ -0,0 +1,13 @@
+// package AAAAA;
+
+public class UnderstandingInterf {
+    public static void main(String[] args) {
+        System.out.println("pran nath babaji");
+    }
+}
+
+interface A{
+    void m(){
+
+    }
+}
