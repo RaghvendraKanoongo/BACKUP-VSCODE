f460fa64cb0f08eb158100573fb8469eaa5a8b06
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-09 01:52:03 +0530

    update

---
+++ AAAAA/Understandingclasses.java
@@ -0,0 +1,17 @@
+// package AAAAA;
+
+public class Understandingclasses {
+    public static void main(String[] args) {
+        System.out.println("babaji");
+    }
+}
+class A{
+    void hola(){
+        System.out.println("IN A");
+    }
+}
+class B extends A{
+    int hola(){
+        System.out.println("in B");
+    }
+}
