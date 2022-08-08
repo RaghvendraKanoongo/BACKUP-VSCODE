0bbe3b401edbab07d5ffd397ab0c4aa2731cef17
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-09 00:15:21 +0530

    update

--- SortingAlgos/MergeSort.java
+++ SortingAlgos/MergeSort.java
@@ -3,16 +3,30 @@
 public class MergeSort {
     public static void main(String[] args) {
         System.out.println("babaji");
+        A a = new B();
+        
     }
 
 }
 
 class A{
+    static{
+        System.out.println("in Static Block");
+    }
+    {
+        System.out.println("in A's instance block");
+    }
     static void hola(){
         System.out.println("in static A");
     }
     void hola1(){
         System.out.println("In non static A");
+    }
+
+}
+class  B extends A{
+    static{
+        System.out.println("IN b's static block");
     }
 
 }
