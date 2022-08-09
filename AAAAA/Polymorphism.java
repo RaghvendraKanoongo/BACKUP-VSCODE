bb09ff6973f7c5eb5b43e76a492bd910d2948cd0
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-09 23:37:55 +0530

    update

--- AAAAA/Polymorphism.java
+++ AAAAA/Polymorphism.java
@@ -3,15 +3,16 @@
 public class Polymorphism {
     public static void main(String[] args) {
         System.out.println("babaji");
+        
     }
 }
 class A{
-    void hola(){
+    Object hola(){
 
     }
 }
 class B extends A{
-    int hola(){
-        
+    String hola(){// here it is co varient as string is child of Object there B can change return type 
+
     }
 }
