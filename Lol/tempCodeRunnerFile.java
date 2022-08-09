85c980c2dacc815a8e4f167fb4c04888e80b2204
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-09 09:36:37 +0530

    update

--- Lol/tempCodeRunnerFile.java
+++ Lol/tempCodeRunnerFile.java
@@ -4,20 +4,9 @@
  * tempCodeRunnerFile
  */
 public class tempCodeRunnerFile {
+    
     public static void main(String[] args) {
-        int[] arr = {-6,6,-6, 6, -6, 6, -6, 6 ,-6, 6, -6};
-         System.out.println( solve(11, arr));
-    }
-
-    public static String solve(int N, int[] arr) {
-        int sum = 0;
-        for(int i = 0; i<N; i++){
-            sum += arr[i];
-        }
-        if(sum==0)
-            return "YES";
-        else 
-            return "NO";
+        
     }
     
 }
