c00867cd5438b1013fe65de72ce7742abe6a4af0
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-09 23:35:53 +0530

    update

---
+++ AAAAA/Polymorphism.java
@@ -0,0 +1,17 @@
+// package AAAAA;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        System.out.println("babaji");
+    }
+}
+class A{
+    void hola(){
+
+    }
+}
+class B extends A{
+    int hola(){
+        
+    }
+}
