710d81375594c9866e97da66ab3033dc896c3cf2
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-10 11:11:45 +0530

    update

--- AAAAA/Polymorphism.java
+++ AAAAA/Polymorphism.java
@@ -13,10 +13,15 @@ class A{
     void h1(){
 
     }
+    int h1(double i){
+
+    }
 }
 class B extends A{
     String hola(){// here it is co varient as string is child of Object there B can change return type 
 
     }
-    
+    private int h1(int i){
+
+    }
 }
