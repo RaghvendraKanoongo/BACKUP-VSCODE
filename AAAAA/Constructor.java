cd37d08f911f9c1ac30f77ba816eab22a5f53b77
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-11 12:04:57 +0530

    update

--- AAAAA/Constructor.java
+++ AAAAA/Constructor.java
@@ -3,11 +3,22 @@
 public class Constructor {
     public static void main(String[] args) {
         System.out.println("babaji");
+        Bb b = new Bb();
     }
     Constructor(){
         
     }
     Constructor(int i){
         this();
+    }
+}
+class Aa{
+    Aa(){
+        System.out.println("in Aa Constructor");
+    }
+}
+class Bb extends Aa{
+    Bb(){
+        System.out.println("In Bb");
     }
 }
