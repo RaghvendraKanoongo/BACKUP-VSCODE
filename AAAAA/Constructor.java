d30619ebb64cb2fd8828758f251ff4f4cf484cc5
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-11 15:48:06 +0530

    update

--- AAAAA/Constructor.java
+++ AAAAA/Constructor.java
@@ -51,3 +51,14 @@ abstract class Cc{
 class Dd extends Cc{
 
 }
+
+class Test
+{
+    private static Test t=null; 
+    private Test(){}
+public static Test getTest()
+     {
+{
+//getTest() method is a factory method
+  if(t==null)
+        t=new Test();
