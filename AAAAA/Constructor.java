4000bdc4163235ed987b8f52b5c9e3d406faaf94
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-11 12:00:56 +0530

    update

---
+++ AAAAA/Constructor.java
@@ -0,0 +1,13 @@
+// package AAAAA;
+
+public class Constructor {
+    public static void main(String[] args) {
+        System.out.println("babaji");
+    }
+    Constructor(){
+        
+    }
+    Constructor(int i){
+        this();
+    }
+}
