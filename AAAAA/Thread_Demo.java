cc869912d8bc20c665dedb756d8ed2c3cdb1595f
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-12 09:36:55 +0530

    update

--- AAAAA/Thread_Demo.java
+++ AAAAA/Thread_Demo.java
@@ -3,11 +3,17 @@
 public class  Thread_Demo {
     public static void main(String[] args) {
         System.out.println("babaji");
+
+        MyThread mt = new MyThread();
+        mt.run();
+        for (int i = 0; i <5; i++) {
+            System.out.println("in void main");
+        }
     }
 }
 
 class MyThread extends Thread{
-    void run(){
+    public void run(){
         System.out.println("inside MyThread's Run");
     }
 }
