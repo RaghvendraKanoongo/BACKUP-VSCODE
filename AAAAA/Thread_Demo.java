cc6c8dadfa750a6e3daa3903f5d23a3ee6d9ce88
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-14 09:59:07 +0530

    update

--- AAAAA/Thread_Demo.java
+++ AAAAA/Thread_Demo.java
@@ -20,3 +20,13 @@ class MyThread extends Thread{
         }
     }
 }
+
+class TempThread implements Runnable{
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+}
