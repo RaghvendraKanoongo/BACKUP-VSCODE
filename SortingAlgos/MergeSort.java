5d5388edad9d26ddb93358fa6a101bdd705854ce
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-16 09:30:58 +0530

    update

--- SortingAlgos/MergeSort.java
+++ SortingAlgos/MergeSort.java
@@ -3,8 +3,14 @@
 public class MergeSort {
     public static void main(String[] args) {
         System.out.println("babaji");
-        
+        int[] arr = {3,5,2,6,1,8,4,9,7};
+
     }
+    int[] mergeSort(int[] arr){
+        
+        return new int[2];
+    }
+
 
 }
 
