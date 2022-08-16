23a2de2d03f70299c16b189d0b8b501425bd9dce
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-16 14:13:37 +0530

    update

--- SortingAlgos/tempCodeRunnerFile.java
+++ SortingAlgos/tempCodeRunnerFile.java
@@ -1,10 +1,43 @@
-for (int i = 0; i < arr.length;) {
-        //     if(arr[i] == 0){
-        //         i++;
-        //     }
-        //     else{
-        //         char ch =(char) (97 + i);
-        //         sortedString += "" + ch;
-        //     }
-        // }
-        // System.out.println(sortedString);
+// package SortingAlgos;
+
+/**
+ * tempCodeRunnerFile
+ */
+public class tempCodeRunnerFile {
+	public static void main(String[] args) {
+		int[] arr = {,9,7,5,3,6};
+
+	}
+	static int[] merge(int[] arr, int s, int m, int e){
+        int[] sortedArr = new int[m+e];
+        int i = s;
+        int j = m+1;
+        int k = 0;
+        while(i<=m && j<=e){
+            if(arr[i]<arr[j]){
+                sortedArr[k] = arr[i];
+                i++;
+                k++;
+            }
+            else if(arr[i]>arr[j]){
+                sortedArr[k] = arr[j];
+                j++;
+                k++;
+            }
+            else if(arr[i] == arr[j]){
+                sortedArr[k] = arr[i];
+                i++;
+                j++;
+                k++;
+            }
+        }
+        while(i<=m){
+            sortedArr[k] = arr[i];
+            i++;
+        }
+        while(j<=e){
+            sortedArr[k] = arr[j];
+            j++;
+        }
+    }
+}
