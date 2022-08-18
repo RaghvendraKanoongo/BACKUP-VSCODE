9bacf8a94769555677bdb6cb22c7d68f95076911
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-18 10:56:49 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,15 +1,16 @@
 /**
  * tempCodeRunnerFile
  */
-package vs_code;
+// package vs_code;
 
 public class tempCodeRunnerFile {
 
     public static void main(String[] args) {
-        int a = 3,b = 5;
-        if((a & (b+1)) == 1){
-            a = a+a;
+        int a = 9, b = 11;
+        if(b%a < a){
+            {
+                b = b >> 1;
+            }
         }
-        else 
     }
 }
