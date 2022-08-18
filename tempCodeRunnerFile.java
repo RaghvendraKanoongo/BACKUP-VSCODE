0344f87ff980a4e0ccfa75c407157afb082a12fd
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-18 10:54:48 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -6,15 +6,10 @@ package vs_code;
 public class tempCodeRunnerFile {
 
     public static void main(String[] args) {
-        int v = 200;
-        int w = 540;
-       System.out.println("babaji");
-        for (int i = 0; i < 540; i++) {
-            w -= i*4;
-            if((w/2)+i == v){
-                System.out.println("tw:"+w/2);
-                System.out.println("fw:"+i);
-            }
+        int a = 3,b = 5;
+        if((a & (b+1)) == 1){
+            a = a+a;
         }
+        else 
     }
 }
