f1c522d1717f8d0ef01dc45e19faf3ec883aed02
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-18 10:58:51 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -6,11 +6,21 @@
 public class tempCodeRunnerFile {
 
     public static void main(String[] args) {
-        int a = 9, b = 11;
+        System.out.println(h(1, 2));
+    }
+    static int h(int a, int b){
+         a = 9;
+         b = 11;
         if(b%a < a){
             {
                 b = b >> 1;
             }
         }
+        else if(a%b<b){
+            {
+                b = b << 1;
+            }   
+        }
+        else if return a%b;
     }
 }
