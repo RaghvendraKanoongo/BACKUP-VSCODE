79b371ae5d7fd2c39376d7aff99531be0dfb345b
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-21 20:57:58 +0530

    update

--- OLD/tempCodeRunnerFile.java
+++ OLD/tempCodeRunnerFile.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 // package OLD;
 
 /**
@@ -6,27 +8,7 @@
 public class tempCodeRunnerFile {
 
 	public static void main(String[] args) {
-		System.out.println("babaji");
-		int[] arr = {1,1,1,3};
-		int countEven = 0;
-        int countOdd = 0;
-        for(int i = 0;i<arr.length; i++){
-            
-            if(arr[i]%2 == 0){
-                countEven++;
-            }
-            else 
-                countOdd++;
-        }
-        if(countOdd+countEven%2 == 1){
-            System.out.println(false);
-        }
-        else if(countEven%2 == 0 && countOdd %2 == 0){
-            
-            System.out.println(true);
-        }
-        else 
-		System.out.println(false);
+		HashMap <Integer> map = new
 
 	}
 }
