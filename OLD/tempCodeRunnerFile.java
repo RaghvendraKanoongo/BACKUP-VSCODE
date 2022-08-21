dfe8e774badc9a51bd06f6215805682ccfed8e9b
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-21 20:27:53 +0530

    update

--- OLD/tempCodeRunnerFile.java
+++ OLD/tempCodeRunnerFile.java
@@ -5,5 +5,28 @@
  */
 public class tempCodeRunnerFile {
 
-	
+	public static void main(String[] args) {
+		System.out.println("babaji");
+		int[] arr = {1,1,1,3};
+		int countEven = 0;
+        int countOdd = 0;
+        for(int i = 0;i<arr.length; i++){
+            
+            if(arr[i]%2 == 0){
+                countEven++;
+            }
+            else 
+                countOdd++;
+        }
+        if(countOdd+countEven%2 == 1){
+            System.out.println(false);
+        }
+        else if(countEven%2 == 0 && countOdd %2 == 0){
+            
+            System.out.println(false);
+        }
+        else 
+		System.out.println(false);
+
+	}
 }
