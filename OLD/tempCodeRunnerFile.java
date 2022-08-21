03b9dc622ca0bd02e69fb81663d764d1486f4161
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-21 21:02:02 +0530

    update

--- OLD/tempCodeRunnerFile.java
+++ OLD/tempCodeRunnerFile.java
@@ -8,7 +8,19 @@ import java.util.HashMap;
 public class tempCodeRunnerFile {
 
 	public static void main(String[] args) {
-		HashMap map = new HashMap<>();
-		
+		int arr[] = {18,6,24,1};
+		HashMap map1 = new HashMap<>();
+		HashMap map2 = new HashMap<>();
+		for (int i = 0; i < arr.length; i++) {
+			if(map1.contains(arr[i]))
+		}
+
+	}
+	long hcf(int n1, int n2)
+	{
+    	if (n2 != 0)
+        	return hcf(n2, n1 % n2);
+    	else
+        	return n1;
 	}
 }
