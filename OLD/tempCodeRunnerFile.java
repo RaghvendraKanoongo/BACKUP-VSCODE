a75c9a5c0e4031fc011a45f0e01cbc4401327e7f
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-21 21:38:26 +0530

    update

--- OLD/tempCodeRunnerFile.java
+++ OLD/tempCodeRunnerFile.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class tempCodeRunnerFile {
 
 	public static void main(String[] args) {
-		int arr[] = {6,5,8,6,5,4};
+		int arr[] = {6,5,8,6,5,4};//
 		HashMap<Integer,Integer> map1 = new HashMap<>();
 
 		for (int i = 0; i < arr.length; i++) {
