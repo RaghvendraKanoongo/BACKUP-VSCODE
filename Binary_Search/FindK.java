45a1e6cfeea13834d5b1a4b3803ef90fa9bd2c61
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-25 21:50:09 +0530

    update

---
+++ Binary_Search/FindK.java
@@ -0,0 +1,10 @@
+// package Binary_Search;
+
+public class FindK {
+    public static void main(String[] args) {
+        System.out.println("babaji");
+        int[] arr = {3,6,9,12,14,19,20,23,25,27};
+        int k = 25;
+        
+    }
+}
