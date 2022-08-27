47252899fecccc61689d0e81b50904c2fe40a25d
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-27 20:10:23 +0530

    update

---
+++ Binary_Search/Solution.java
@@ -0,0 +1,30 @@
+// package Binary_Search;
+
+public class Solution {
+    static int[] arr ={0,0,0,0,0,1,1,1,1};
+
+    static boolean isBadVersion(int n){
+        if(arr[n]==1)return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstBadVersion(5)); 
+    }
+
+    static public int firstBadVersion(int n) {
+    
+        int start = 0,end = n-1,mid = (start + end)/2;
+        while(end - start >=1 ){
+            if(isBadVersion(start+1)){return start + 2;}
+                if(isBadVersion(mid)){
+                 end = mid;
+                }
+                else{
+                    start = mid;
+                }
+                mid = (start + end)/2;
+        }
+        return start+1;
+    }
+}
