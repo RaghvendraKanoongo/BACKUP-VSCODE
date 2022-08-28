fc7cba2d79a42752360bd68cd33b2a1a4db4e65f
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-28 16:16:19 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -2,47 +2,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-// /**
-//  * tempCodeRunnerFile
-//  */
-// // package vs_code;
-
-// public class tempCodeRunnerFile {
-
-//     public static void main(String[] args) {
-//         System.out.println(h(1, 2));
-//         int x = 1;
-//         int[][] arr = {{12,21},{13,54},{52,63}};
-//         for (int i = 0; i < 3; i++) {
-//             for (int j = 0; j < i; j++) {
-//                 x = x+arr[i][j];
-//             }
-//         }
-//         System.out.println(x);
-//     }
-//     static int h(int a, int b){
-//          a = 9;
-//          b = 11;
-//         if(b%a < a){
-//             {
-//                 b = b >> 1;
-//                 return a;
-//             }
-//         }
-//         else if(a%b<b){
-//             {
-//                 b = b << 1;
-//                 return b;
-//             }   
-//         }
-//         else  
-//         return a%b;
-//     }
-// }
-
-/**
- * tempCodeRunnerFile
- */
 public class tempCodeRunnerFile {
     public static void main(String[] args) {
 
@@ -58,7 +17,7 @@ public class tempCodeRunnerFile {
             arr2[i] = sc.nextInt();
         }
 
-        solve(arr1, arr2, n);
+        System.out.println(solve(arr1, arr2, n));
 
     }
 
@@ -74,7 +33,6 @@ public class tempCodeRunnerFile {
         int N = arr1.length;
         int M = arr2.length;
  
-        // If lengths of arrays are not equal
         if (N != M)
             return false;
  
