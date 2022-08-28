fe998d38cb875d1d2f54323829b2e3b572b11b4d
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-28 16:48:39 +0530

    update

--- Temp.java
+++ Temp.java
@@ -5,67 +5,16 @@ import java.util.Scanner;
 
 public class Temp {
  
-    static class node {
-        int val;
-        node next;
-    };
- 
-    static node head = null;
-    static int count(node head)
+    public static LinkedList<String> reverseLinkedList(LinkedList<Integer> llist)
     {
-        node p = head;
-        int k = 1;
-        while (p != null) {
-            p = p.next;
-            k++;
+        LinkedList<Integer> revLinkedList = new LinkedList<Integer>();
+        for (int i = llist.size() - 1; i >= 0; i--) {
+  
+            // Append the elements in reverse order
+            revLinkedList.add(llist.get(i));
         }
-        return k;
-    }
- 
-    static node ll_reverse(node head)
-    {
-        node p = head;
-        int i = count(head), j = 1;
-        int[] arr = new int[i];
-        while (i != 0 && p != null) {
-            arr[j++] = p.val;
-            p = p.next;
-            i--;
-        }
-        j--;
-        while (j != 0)
-            System.out.print(arr[j--] + " ");
-        return head;
-    }
-
-    static node insert_end(node head, int data)
-    {
-        node q = head;
-        node p = new node();
-        p.val = data;
-        p.next = null;
-        while (q.next != null)
-            q = q.next;
-        q.next = p;
-        p.next = null;
-        return head;
-    }
- 
-    // create ll
-    static node create_ll(node head, int data)
-    {
-        node p = new node();
-        p.next = null;
-        p.val = data;
-        if (head == null) {
-            head = p;
-            p.next = null;
-            return head;
-        }
-        else {
-            head = insert_end(head, data);
-            return head;
-        }
+        // Return the reversed arraylist
+        return revLinkedList;
     }
  
     public static void main(String[] args)
@@ -80,7 +29,12 @@ public class Temp {
         for (int i = 0; i < n; i++) {
             ll1.add(sc.nextInt());
         }
+        for (int i = 0; i < n; i++) {
+            ll2.add(sc.nextInt());
+        }
+
         LinkedList<String> ll = new LinkedList<String>();
+        ll1 = reverseLinkedList(ll1);
         
     }
 
