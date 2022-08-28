272e3e30c325eda0ded7c7028ec849c47ab54da2
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-28 16:10:14 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 // /**
 //  * tempCodeRunnerFile
@@ -42,8 +43,16 @@ import java.util.HashMap;
  * tempCodeRunnerFile
  */
 public class tempCodeRunnerFile {
+    public static void main(String[] args) {
+        
+        if(areEqual(arr1, arr2, n))
+            return 1;
+        else 
+            return 0;
 
-    public static boolean areEqual(int arr1[], int arr2[])
+    }
+
+    public static boolean areEqual(int arr1[], int arr2[], int n)
     {
         int N = arr1.length;
         int M = arr2.length;
@@ -52,8 +61,6 @@ public class tempCodeRunnerFile {
         if (N != M)
             return false;
  
-        // Store arr1[] elements and their counts in
-        // hash map
         Map<Integer, Integer> map
             = new HashMap<Integer, Integer>();
         int count = 0;
@@ -67,18 +74,12 @@ public class tempCodeRunnerFile {
             }
         }
  
-        // Traverse arr2[] elements and check if all
-        // elements of arr2[] are present same number
-        // of times or not.
         for (int i = 0; i < N; i++) {
  
-            // If there is an element in arr2[], but
-            // not in arr1[]
+   
             if (!map.containsKey(arr2[i]))
                 return false;
- 
-            // If an element of arr2[] appears more
-            // times than it appears in arr1[]
+
             if (map.get(arr2[i]) == 0)
                 return false;
  
