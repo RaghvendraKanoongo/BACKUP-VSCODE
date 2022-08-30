818f662576e0a8cf20162b92ef7c6ce50f126eeb
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-30 09:04:25 +0530

    update

--- AAAAA/Lemda.java
+++ AAAAA/Lemda.java
@@ -3,8 +3,11 @@
 public class Lemda {
     public static void main(String[] args) {
         DemoLemda obj = new DemoLemda() {
-            
+            public void show() {
+               System.out.println("This is Show");
+            }
         };
+        obj.show();
     }
 }
 
