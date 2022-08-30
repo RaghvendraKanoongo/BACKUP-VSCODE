139f542eeaf2d4fe0a17f9704e00ce3ec619e804
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-30 09:02:23 +0530

    update

---
+++ AAAAA/Lemda.java
@@ -0,0 +1,15 @@
+// package AAAAA;
+
+public class Lemda {
+    public static void main(String[] args) {
+        DemoLemda obj = new DemoLemda() {
+            
+        };
+    }
+}
+
+
+
+interface DemoLemda {
+    void show();
+}
