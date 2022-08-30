e1b6a50b2a3c3ea1298de46f39df1ee81b533622
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-30 16:29:33 +0530

    update

--- AAAAA/A1.java
+++ AAAAA/A1.java
@@ -2,7 +2,16 @@
 
 public class A1 {
     
-    
+    public static void main(String[] args) {
+        A1 a1 = new A1();
+        a1.m1(123.3f);
+    }
+    static void m1(int i){
+        System.out.println("inside int");
+    }
+    static void m1(float i){
+        System.out.println("inside float");
+    }
 }
 class A{
     void m1(int i){
