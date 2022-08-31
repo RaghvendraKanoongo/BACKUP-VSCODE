94c005a52cf431fe410f26ee2ca6d5c4a4fe53ee
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-31 22:55:35 +0530

    update

--- Temp.java
+++ Temp.java
@@ -1,54 +1,20 @@
-import java.util.LinkedList;
-import java.util.Scanner;
-
-
+/**
+ * Temp
+ */
 public class Temp {
- 
-    public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> llist)
-    {
-        LinkedList<Integer> revLinkedList = new LinkedList<Integer>();
-        for (int i = llist.size() - 1; i >= 0; i--) {
-  
-            // Append the elements in reverse order
-            revLinkedList.add(llist.get(i));
+
+    public static void main(String[] args) {
+        
+    }
+    static int hola(){
+        try {
+            System.out.println(10/0);
+            return 1;
+        } catch (Exception e) {
+            return 2;
         }
-        // Return the reversed arraylist
-        return revLinkedList;
-    }
- 
-    public static void main(String[] args)
-    {
+        finally {
 
-
-        LinkedList<Integer> ll1 = new LinkedList<Integer>();
-        LinkedList<Integer> ll2 = new LinkedList<Integer>();
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            ll1.add(sc.nextInt());
         }
-        // for (int i = 0; i < n; i++) {
-        //     ll2.add(sc.nextInt());
-        // }
-
-        ll1 = reverseLinkedList(ll1);
-        System.out.println(ll1);
-        ll2 = reverseLinkedList(ll2);
-
-        static int a2 = 10;
-
-
     }
-
-    static void m1(int a, boolean b){
-        System.out.println("you are inside the m1 method");
-    }
-
-}
-class A1{
-    int a = 1;
-}
-class B extends A1{
-     int a = 2;
 }
