a9342ddea28493100b222b4f4d22316b6b75e3e1
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-08-31 22:57:37 +0530

    update

--- Temp.java
+++ Temp.java
@@ -4,17 +4,19 @@
 public class Temp {
 
     public static void main(String[] args) {
-        
+        System.out.println(hola());
     }
     static int hola(){
         try {
             System.out.println(10/0);
             return 1;
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
             return 2;
-        }
+        } 
         finally {
-
+            System.out.println("hi");
+            return 3;
         }
     }
 }
