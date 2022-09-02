5acee7997e9ba5384eb06c9a852a6d731804fa52
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-02 19:22:22 +0530

    update

--- Temp.java
+++ Temp.java
@@ -6,16 +6,16 @@ public class Temp {
     public static void main(String[] args) {
        int a = 10;
        int b = 20;
-    
+       cal(a,b);
     } 
 
-    static void cal(){
+    static void cal(int a, int b){
         System.out.println(a+b);
     }
     static void pal(){
         System.out.println(a+b);
     }
-    static void pal(){
+    static void gpal(){
         System.out.println(a+b);
     }
     
