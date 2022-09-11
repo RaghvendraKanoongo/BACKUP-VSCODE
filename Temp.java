80b8cf7da35bd8dd4aea93a842b92a8bcf7c72e6
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-11 13:44:42 +0530

    update

--- Temp.java
+++ Temp.java
@@ -1,11 +1,14 @@
 class Temp {
     public static void main(String[] args) {
-        
+        System.out.println( funn(7, 5));
     }
     static int funn(int a, int b){
-        if((a^b )<(a+b ) && (b-1 )<(a-b)){
+        if((b^a )<(a+b ) && (b-a )<(a-b)){
             b = a+2;
-            a = 2+3+
+            a = 2+3+b;
+            return a+b+funn(b,a)-a;
         }
+        else 
+        return a+1;
     }
 }
