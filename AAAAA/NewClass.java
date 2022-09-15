974e19d40e978d126ec2cfd794b6cc33a3f54f1a
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-15 23:49:23 +0530

    update

--- AAAAA/NewClass.java
+++ AAAAA/NewClass.java
@@ -2,27 +2,29 @@
 
 public class NewClass {
     public static void main(String[] args) {
-        Parent p1 = new Parent();
-        p1.sayHi();
-        Child c1 = new Child();
-        c1.sayHi();
+        // Parent p1 = new Parent();
+        // p1.sayHi();
+        // Child c1 = new Child();
+        // c1.sayHi();
         Parent p3 = new Child();
         p3.sayHi();
-        
+
     }
 }
 
 class Parent{
-    void sayHi(){
+    static void sayHi(){
         System.out.println("hi from Parent");
     }
 }
 class Child extends Parent{
     int a;
-    // void sayHi(){
-    //     System.out.println("hi from Child");
-    // }
-    void inChild(){
-        System.out.println("i am a child");
+
+    static void  sayHi(){
+        System.out.println("hi from Child");
     }
+
+    // void inChild(){
+    //     System.out.println("i am a child");
+    // }
 }
