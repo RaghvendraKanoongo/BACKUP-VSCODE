ff557562068a2d6eccb594d6e4d474356cd232df
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-15 23:39:13 +0530

    update

---
+++ AAAAA/NewClass.java
@@ -0,0 +1,16 @@
+// classpackage AAAAA;
+
+public class NewClass {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Parent{
+    void sayHi(){
+        sysout
+    }
+}
+class Child{
+
+}
