38d09b6a9e75cec7734ece4a0a49ef005ba89435
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-20 14:38:40 +0530

    update

--- Binary_Search/CountFrequency.java
+++ Binary_Search/CountFrequency.java
@@ -1,5 +1,10 @@
 // package Binary_Search;
 
 public class CountFrequency {
-    
+    public static void main(String[] args) {
+        System.out.println("babaji");
+    }
+    static int count(){
+        
+    }
 }
