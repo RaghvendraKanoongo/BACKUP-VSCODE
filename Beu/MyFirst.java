bc1575389fc5360ea2efb6b7876d0c91027fdd91
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-25 18:40:52 +0530

    update

---
+++ Beu/MyFirst.java
@@ -0,0 +1,10 @@
+// package Beu;
+
+public class MyFirst {
+    public static void main(String[] args) {
+        
+    }
+    static void hello(){
+        System.out.println("hi avani");
+    }
+}
