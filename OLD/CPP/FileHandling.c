694276e5f1147ff38c1a4dd538431de2286b4280
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-26 09:14:14 +0530

    update

--- OLD/CPP/FileHandling.c
+++ OLD/CPP/FileHandling.c
@@ -3,7 +3,7 @@ int main()
 {
     FILE *fp;
     char ch;
-    // opening file in read mode
+
     fp = fopen("Raghav.txt", "r");
     while (1)
     {
@@ -13,7 +13,7 @@ int main()
         printf("%c", ch);
     }
     fclose(fp);
-    // writing to file in write mode
+
     fp = fopen("main.txt", "w");
     fprintf(fp, "Hello file by fprintf...\n");
     fclose(fp);
