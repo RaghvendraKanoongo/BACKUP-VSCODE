33aa7b44eb47b00af0f097ef927626486d79727f
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-26 10:07:40 +0530

    update

--- OLD/CPP/FileHandling.c
+++ OLD/CPP/FileHandling.c
@@ -4,15 +4,11 @@ int main()
     FILE *fp;
     char ch;
 
-    
 
-   fp = fopen("main.txt", "r");
-    while (1)
-    {
-        ch = fgetc(fp);
-        if (ch == EOF)
-            break;
-        printf("%c", ch);
-    }
+   fclose(fp);
+
+    fp = fopen("main.txt", "w");
+    fprintf(fp, "Hello file by fprintf...\n");
     fclose(fp);
+    return 0;
 }
