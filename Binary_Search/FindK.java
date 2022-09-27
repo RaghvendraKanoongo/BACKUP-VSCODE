8c4439b504c90eea77d200c3cbbd00f036224538
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-27 23:55:51 +0530

    update

--- Binary_Search/FindK.java
+++ Binary_Search/FindK.java
@@ -4,8 +4,10 @@ public class FindK {
     public static void main(String[] args) {
         System.err.println("babaji");
         //           0 1 2 3 4 5 6 7 8 9  10 11
+        //   1
+        // -3-1 0 
         int[] arr = {2,5,8,10,12,15,18,50,80,90};
-                //                      ||
+                //                |  |
         int k = 50;// to find 
         int low = 0;
         int high = arr.length-1;
@@ -23,7 +25,7 @@ public class FindK {
             if(high == low && arr[low] != k){
                 break;
             }
-            
+
             if(arr[mid]>k){
                 high = mid;
             }
