1b28354fb29543f88f0829eca47b8f66e31fb5aa
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-09-29 19:39:39 +0530

    update

--- Binary_Search/CountFrequency.java
+++ Binary_Search/CountFrequency.java
@@ -9,24 +9,27 @@ public class CountFrequency {
         int high = arr.length-1;
         int mid = (low/2) + (high/2);
         int start = 0;
-
+        int end = arr.length-1;
         if(arr[low] == key);
             start = low;
-            
-        while(true){
-            if(arr[mid] == key)
-                high = mid;
-            else if (arr[mid] < key)
-                low = mid;
-            else if(arr[mid] > key)
-                high = mid;
-            if(low+1 == high){
-                start = high;
-                break;
-            }
-            mid = (low/2) + (high/2);
-        }
 
+        else{
+
+            while(true){
+                if(arr[mid] == key)
+                    high = mid;
+                else if (arr[mid] < key)
+                    low = mid;
+                else if(arr[mid] > key)
+                    high = mid;
+                if(low+1 == high){
+                    start = high;
+                    break;
+                }
+                mid = (low/2) + (high/2);
+            }
+        }
+    
         return new int[4];
     }
 }
