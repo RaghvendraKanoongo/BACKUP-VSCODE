9f089f9b3c454a0ada7090fe1cc4f7b159102793
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-10-11 22:22:28 +0530

    update

--- LL/Test.java
+++ LL/Test.java
@@ -1,9 +1,19 @@
+import java.util.LinkedList;
+
 // package LL;
+
+class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 
 public class Test {
     public static void main(String[] args) {
         System.out.println("hola");
     }
 
-    
+    static LinkedList 
 }
