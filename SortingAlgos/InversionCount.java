28c558884489996052da2cebaff2fe90ce313eed
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-10-13 10:00:27 +0530

    update

--- SortingAlgos/InversionCount.java
+++ SortingAlgos/InversionCount.java
@@ -1,5 +1,12 @@
 // package SortingAlgos;
 
 public class InversionCount {
-    
+    static int count = 0;
+    public static void main(String[] args) {
+        System.out.println("babaji");
+    }
+
+    static void divide(int[] arr, int l, int high){
+        int m = 
+    }
 }
