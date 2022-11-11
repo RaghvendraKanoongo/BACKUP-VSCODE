533765628e254d0509fbecf888172510fea6e609
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-11-11 22:28:23 +0530

    update

--- Temp.java
+++ Temp.java
@@ -6,9 +6,12 @@ class Temp {
         String s = "httpssunrux";
         String bagBegin = s.substring(0,4);
         System.out.println(bagBegin);
-        String end = s.substring(
+        String end = s.substring(s.length()-1);
+        String end2 = s.substring(s.length()-3,s.length()-1);
+System.out.println(end2);
+
             
-        )
+
 
         // return (bagBegin+"://"+;
         
