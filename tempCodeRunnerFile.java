a415c0fe482fdb97f171ea4caeb239281da6b962
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-11-22 18:27:29 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -4,9 +4,7 @@ public class tempCodeRunnerFile {
     public static void main(String[] args) {
         int i = 9;
         // while (true) {
-            if(i == 9){
-                continue;
-            }
+            
     } 
 }
 
