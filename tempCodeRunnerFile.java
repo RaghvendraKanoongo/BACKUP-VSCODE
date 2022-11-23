b000a13f8eb9e26b7aa888391896bd617f1150cc
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-11-23 16:12:37 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -4,7 +4,13 @@ public class tempCodeRunnerFile {
     public static void main( String args[] ) {
         int dec = 161;
         // converting to binary and representing it in a string
+        Arraylist<Integer> arr = new Arraylist<>();
         String bin = Integer.toBinaryString(dec);
         System.out.println(bin);
+        int n = bin.length();
+        for (int i = 0; i < n; i++) {
+            if
+        }
+
     }
 }
