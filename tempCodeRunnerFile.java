a6bfd58456822eafd6987c7816c7b2102cd94620
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-11-23 16:06:34 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,16 +1,11 @@
 
 public class tempCodeRunnerFile {
 
-    public static void main(String[] args) {
-        int i = 9;
-    } 
-}
-
- class A{
-    public static void main(String[] args) {
+    public static void main( String args[] ) {
+        int dec = 161;
+        // converting to binary and representing it in a string
+        String bin = Integer.toBinaryString(dec);
+        System.out.println(bin);
         
-    }
-    void foo(){
-        System.out.println("in A");
-    }
+     }
 }
