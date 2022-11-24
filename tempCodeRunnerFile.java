e1b23ea31820bc0fe2da1690b20e535e9945015a
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-11-24 08:25:50 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -3,8 +3,6 @@ public class tempCodeRunnerFile {
 
     public static void main( String args[] ) {
         int dec = 161;
-        // converting to binary and representing it in a string
-       
         String bin = Integer.toBinaryString(dec);
         System.out.println(bin);
         int n = bin.length();
