c2b1eea03fb231cb822eae28849c64c9fa76f6a8
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-12-05 00:49:10 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -16,6 +16,7 @@ public class tempCodeRunnerFile {
                 arrlist.add(i+1);
             }
         }
+        
         int x = arrlist.size();
         arrlist.add(0,x);
         System.out.println(arrlist);
