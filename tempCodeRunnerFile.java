2a91d5931b500fe54b71b61ac19f95b1e2ab5b63
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-12-05 21:48:23 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 public class tempCodeRunnerFile {
-
     public static void main(String args[]) {
         int dec = 161;
         String bin = Integer.toBinaryString(dec);
@@ -9,7 +8,7 @@ public class tempCodeRunnerFile {
         System.out.println("here");
         System.out.println(13&4);
         ArrayList<Integer> arrlist = new ArrayList<Integer>();
-        
+
         for (int i = 0; i < n; i++) {
             char a = bin.charAt(i);
             if(a == '1'){  
