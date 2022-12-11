22f3137f490eb6b5e063836abf0e02ef801f17f6
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-12-11 16:39:46 +0530

    update

--- Temp.java
+++ Temp.java
@@ -6,20 +6,20 @@ public class Temp {
 	static ArrayList<String> list = new ArrayList<String>();
 	public static void main(String[] args) {
 		String str = new String();
-        pGenerator(str, 2, 2);
+        pGenerator(str, 3, 3);
 		System.out.println(list);
 	}
     //[ (()), ((), ()), (), )(), )(, )), )]
     public static void pGenerator(String str, int oCount,int cCount){
 		
 		if(oCount != 0){
-			str = str+")";
-			pGenerator(str+"(", --oCount,cCount);
+			str = str+"(";
+			pGenerator(str, --oCount,cCount);
 		}
 
 		if(cCount != 0){
 			str = str+")";
-			pGenerator(str+")", oCount,--cCount);
+			pGenerator(str, oCount,--cCount);
 		}
 
 		if(cCount == 0 && oCount == 0){
