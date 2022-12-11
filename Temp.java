4cdf869824cc0ef6820e295652eb6b1147776974
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-12-11 15:29:14 +0530

    update

--- Temp.java
+++ Temp.java
@@ -1,7 +1,24 @@
-/**
- * Temp
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Temp {
 
-	
+	static ArrayList<String> list = new ArrayList<String>();
+	public static void main(String[] args) {
+		String str = new String();
+        pGenerator(str, 3, 3);
+	}
+    
+    public static void pGenerator(String str, int oCount,int cCount){
+
+        if(oCount == 1){
+            return;
+        }
+        if(cCount == 1){
+            list.add(str);
+            return;
+        }
+        pGenerator(str.add("("), oCount-1,cCount);
+        pGenerator(str.add(')'), oCount,cCount-1);
+    }
 }
