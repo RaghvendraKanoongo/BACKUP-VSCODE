72500414a03116c54231bc92e262a325f53d601d
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2022-12-11 17:56:22 +0530

    update

--- Temp.java
+++ Temp.java
@@ -6,18 +6,18 @@ public class Temp {
 	static ArrayList<String> list = new ArrayList<String>();
 	public static void main(String[] args) {
 		String str = new String();
-        pGenerator(str, 2, 2);
+        pGenerator(str, 3,3);
 		System.out.println(list);
 	}
 
     public static void pGenerator(String str, int oCount,int cCount){
 		if(oCount > 0){
-			str = str+"("; 
-	    	pGenerator(str, oCount-1, cCount);
+			// str = str+"("; 
+	    	pGenerator(str+"(", oCount-1, cCount);
 		}
 		if(cCount > 0){
-			str = str+")";
-			pGenerator(str, oCount, cCount-1);
+			// str = str+")";
+			pGenerator(str+")", oCount, cCount-1);
 		}
 		if(cCount == 0){
 			list.add(str);
