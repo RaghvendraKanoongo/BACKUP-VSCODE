3d2a3388c2836a6105a7ae61db5e6be3f4c15463
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2023-01-26 11:31:16 +0530

    update

--- Stack/NearestSmaller1.java
+++ Stack/NearestSmaller1.java
@@ -8,5 +8,9 @@ public class NearestSmaller1 {
     public static void main(String[] args) {
         System.out.println("babaji");
     }
-    
+
+    public static int[] solve(int[] arr){
+        return 
+    }
+
 }
