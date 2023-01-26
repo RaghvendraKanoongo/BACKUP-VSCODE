75229368ed93bc1afe09937ca7a1ca054f2a10b7
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2023-01-26 11:51:22 +0530

    update

--- Stack/NearestSmaller1.java
+++ Stack/NearestSmaller1.java
@@ -16,11 +16,15 @@ public class NearestSmaller1 {
         Stack <Integer> stack = new Stack<>();
         
         for (int i = 0; i < arr.length; i++) {
-            if(stack.isEmpty()&&i == 0){
+            if(stack.isEmpty() && i == 0){
                 stack.push(arr[i]);
                 arr[i] = -1;
             }
-            else if(arr[i] = )
+            else if(arr[i]>stack.peek()){
+                stack.push(arr[i]);
+                arr[i] = stack.peek();
+            }
+            else
         }
         return new int[3];
     }
