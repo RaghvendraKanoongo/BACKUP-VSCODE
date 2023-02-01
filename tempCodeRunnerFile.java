ff956e036ca2ac3f02492fcc849224a69ca500ed
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2023-02-01 14:23:39 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 public class tempCodeRunnerFile {
     public static void main(String args[]) {
-        System.out.println(args[0]);
+
         if(isEligible(2))
             System.out.println("You are mature");
         else
             System.out.println("Still a kiddooo!!");
-            sayHi();
+            sayHi(args);
     }
     public static boolean isEligible (int age){
         if(age>=18)
