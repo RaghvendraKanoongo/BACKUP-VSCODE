7781bc16fe36e2c4cb78fc22fde970cde7fedbf5
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2023-02-01 13:43:28 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,22 +1,9 @@
 import java.util.ArrayList;
 public class tempCodeRunnerFile {
     public static void main(String args[]) {
-        int dec = 161;
-        String bin = Integer.toBinaryString(dec);
-        System.out.println(bin);
-        int n = bin.length();
-        System.out.println("here");
-        System.out.println(13&4);
-        ArrayList<Integer> arrlist = new ArrayList<Integer>();
+        
+    }
+    public static boolean is18(int age){
 
-        for (int i = 0; i < n; i++) {
-            char a = bin.charAt(i);
-            if(a == '1'){  
-                arrlist.add(i+1);
-            }
-        }
-        int x = arrlist.size();
-        arrlist.add(0,x);
-        System.out.println(arrlist);
     }
 }
