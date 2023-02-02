9f4f510b8f61fa7418bcf49f579c53783ba68fc1
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2023-02-02 13:54:15 +0530

    update

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -3,15 +3,16 @@ public class tempCodeRunnerFile {
      int a;
     public static void main(String args[]) {
 
-       tempCodeRunnerFile tempCodeRunnerFile = new tempCodeRunnerFile();
-       System.out.println(tempCodeRunnerFile.a);
+    //    tempCodeRunnerFile tempCodeRunnerFile = new tempCodeRunnerFile();
+    //    System.out.println(tempCodeRunnerFile.a);
+       System.out.println(test());
 
 
-        if(isEligible(2))
-            System.out.println("You are mature");
-        else
-            System.out.println("Still a kiddooo!!");
-            sayHi(args);
+        // if(isEligible(2))
+        //     System.out.println("You are mature");
+        // else
+        //     System.out.println("Still a kiddooo!!");
+        //     sayHi(args);
     }
     public static boolean isEligible (int age){
         if(age>=18)
@@ -20,13 +21,19 @@ public class tempCodeRunnerFile {
     }
     public static void sayHi(String hi[]){
         System.out.println("saying hi ");
-        System.out.println()
     }
 
     static int test(){
 
         try{
-            
+            System.out.println(1/0);
+            return 1;
+        }
+        catch(Exception e){
+            return 2;
+        }
+        finally{
+            return 3;
         }
     }
 }
