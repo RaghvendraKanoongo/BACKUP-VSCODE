90a6ff45604b7b3c0cf1bcf9673c80e081229011
RaghvendraKanoongo <kanoongoraghvendra30@gmail.com>
2023-02-18 02:14:23 +0530

    update

--- LLD/Singloton.java
+++ LLD/Singloton.java
@@ -1,13 +1,24 @@
 // package LLD;
 
 public class Singloton {
+
+    static Singloton instance = new Singloton();
     public static void main(String[] args) {
-        TestSingloton testSingloton;
+        Test test = new Test();
+    }
+    void hello(){
+        System.out.println("hello");
     }
 }
 
-class TestSingloton{
-    static{
-        System.out.println("inside testsingleton");
+class Test {
+    static Test instance = new Test();
+
+    static {
+        System.out.println("Test class loaded.");
+    }
+
+    public Test() {
+        System.out.println("Test instance created.");
     }
 }
